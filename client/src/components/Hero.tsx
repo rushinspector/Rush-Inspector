1d77871954101dee35fd6cb09c21f9428140e0d2
reyjehwhaegre <51313034-reyjehwhaegre@users.noreply.replit.com>
2025-12-13 23:35:06 +0000

    Restored to '4d66903260e5db5a3ce50ebd78961401e4ad49c5'
    Replit-Restored-To: 4d66903260e5db5a3ce50ebd78961401e4ad49c5

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
             
             {/* Desktop version (lg and above only) */}
             <p className="hidden lg:block text-2xl lg:text-[28px] hero-subtitle text-gray-100 max-w-xl mt-10 lg:mt-12 font-medium leading-tight">
-              <span className="lg:whitespace-nowrap">Home inspector matched within 24 hours—</span><br/>
+              <span className="lg:whitespace-nowrap">Home inspector booked within 24 hours—</span><br/>
               pay only when confirmed.
             </p>
             
